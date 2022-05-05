3b267d03da7bf7d4caabdbcf55f054af21f452c5
bwf1dodolo <82048279+bwf1dodolo@users.noreply.github.com>
2022-05-05 07:36:37 +0530

    make it better

--- resources/views/Auth/Register.blade.php
+++ resources/views/Auth/Register.blade.php
@@ -7,6 +7,132 @@
     <title>comfac/RegisterPage</title>
     <link href="{{asset('css/register.css')}}"  rel="stylesheet" >
     <link href="{{asset('css/footer.css')}}" rel="stylesheet">
+    <style>
+        *{
+    margin:0px;
+    padding:0;
+
+}
+.navbar{
+    height:60px;
+    padding:0px 20px;
+    background-color: rgb(145, 9, 9);
+    color:white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.navbar button{
+    padding:10px 20px;
+    font-size:18px;
+    margin:0px 10px;
+    border:none;
+    border-radius: 50px;
+    transition:1s;
+    cursor: pointer;
+}
+.navbar button:hover{
+    background-color:#000000;
+    color:white;
+}
+.navbar a {
+    text-decoration: none;
+    color:black;
+}
+.navbox button:hover a{
+    color:white;
+}
+.register-form{
+        width: 80%;
+        height: 500px;
+        margin:50px auto;
+        padding:20px;
+}
+.register-form form{
+    display: flex;
+    width:30%;
+    margin:auto;
+    padding:30px;
+    flex-direction: column;
+    background-color: rgb(206, 211, 211);
+}
+.register-form form input{
+    height: 40px;
+    margin: 20px 0px;
+    padding:0px 10px;
+    border:none;
+    border-radius: 50px;
+
+}
+input[type="submit"]{
+    background-color: rgb(145, 9, 9);
+    color:white;
+}
+
+
+@media only screen and (max-width:768px){
+    .register-form{
+        min-height:400px;
+    }
+    .navbox button{
+        padding:10px 15px;
+        font-size:14px;
+        margin:0px 5px;
+    }
+    .navbar h1{
+        font-size: 1.6rem;
+    }
+    .navbox h1{
+        font-size:1rem;
+    } 
+    .navbar{
+        padding-left:10px;
+    }
+    .register-form{
+        width:90%;
+        min-height: 500px;
+        margin:20px auto;
+        padding:10px;
+}
+.register-form form{
+    width:60%;
+    padding:30px 20px;
+    }
+
+}
+@media only screen and (max-width:576px){
+     .register-form{
+        width:90%;
+        min-height: 500px;
+        margin:50px auto;
+        padding:10px;
+}
+.register-form form{
+    width:80%;
+    padding:20px;
+    }
+
+}
+
+.footer-div{
+    background-color: bisque;
+    text-align: center;
+    padding:10px 0px;
+}
+.footer-links{
+    display: inline-block;
+    margin:0px 10px;
+    font-size: 20px;
+    font-weight:bolder;
+}
+.footer-intro{
+    margin-top:10px;
+}
+.footer-intro p{
+    margin-top:5px;
+    font-size: 20px;
+}
+    </style>
 
 </head>
 <body>
--- resources/views/Users/Dashboard.blade.php
+++ resources/views/Users/Dashboard.blade.php
@@ -7,7 +7,121 @@
     <title>comfac/RegisterPage</title>
     <link href="{{asset('css/dashboard.css')}}"  rel="stylesheet" >
     <link href="{{asset('css/footer.css')}}" rel="stylesheet">
+    <style>
+        *{
+    margin:0px;
+    padding:0;
 
+}
+.navbar{
+    height:60px;
+    padding:0px 20px;
+    background-color: rgb(145, 9, 9);
+    color:white;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.navbar button{
+    padding:10px 20px;
+    font-size:18px;
+    margin:0px 10px;
+    border:none;
+    border-radius: 50px;
+    transition:1s;
+    cursor: pointer;
+}
+.navbar button:hover{
+    background-color:#000000;
+    color:white;
+}
+.navbar a {
+    text-decoration: none;
+    color:black;
+}
+.navbox button:hover a{
+    color:white;
+}
+
+.login-main{
+    width:80%;
+    min-height:400px;
+    margin:30px auto;
+    padding:40px 0px;
+    text-align: center;
+    background-color: rgb(206, 211, 211);
+}
+.login-main h1{
+    font-size: 3rem;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    margin:20px 0px;
+}
+.login-main h2{
+    margin:20px 0px
+}
+.login-main p,span{
+    margin: 20px 0px;
+    font-family: cursive;
+    font-size: 18px;
+}
+
+/* responsive css  */
+
+
+@media only screen and (max-width:568px){
+    .navbox button{
+        padding:10px;
+        font-size:14px;
+        margin:0px 5px;
+    }
+    .navbar h1{
+        font-size: 1.6rem;
+    }
+    .navbox h1{
+        font-size:1rem;
+    } 
+    .navbar{
+        padding-left:10px;
+    }
+    .login-main{
+        width:95%;
+        min-height:400px;
+        margin:30px auto;
+        padding:40px 0px;
+        text-align: center;
+        background-color: rgb(206, 211, 211);
+    }
+    .navbar h1{
+        font-size: 1.4rem;
+    }
+    .login-main h1{
+        font-size: 2rem;
+    }
+    .login-main p,span{
+        margin: 20px 0px;
+        font-size: 16px;
+    }
+    
+}
+.footer-div{
+    background-color: bisque;
+    text-align: center;
+    padding:10px 0px;
+}
+.footer-links{
+    display: inline-block;
+    margin:0px 10px;
+    font-size: 20px;
+    font-weight:bolder;
+}
+.footer-intro{
+    margin-top:10px;
+}
+.footer-intro p{
+    margin-top:5px;
+    font-size: 20px;
+}
+    </style>
 </head>
 <body>
     <div class="navbar">
